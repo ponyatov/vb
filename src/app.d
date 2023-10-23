bdfe4c9325495b8d7fd97935ef3e033a8bb28e11
Dmitry Ponyatov <dponyatov@gmail.com>
2023-10-24 02:47:57 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/app.d
+++ src/app.d
@@ -1,6 +1,19 @@
 import std.stdio;
 
-void main()
-{
-    writeln("Edit source/app.d to start your project.");
+import vibe.vibe;
+
+void main() {
+    auto settings = new HTTPServerSettings();
+    settings.port = 8888;
+    settings.bindAddresses = ["127.0.0.1"];
+    auto listener = listenHTTP(settings, &hello);
+    scope (exit)
+        listener.stopListening;
+    logInfo("http://127.0.0.1:8888");
+    runApplication();
+}
+
+void hello(HTTPServerRequest req, HTTPServerResponse res) {
+    logInfo("%s\t%s".format(req.method, req.requestURI));
+    res.writeBody("Hello, World!");
 }
