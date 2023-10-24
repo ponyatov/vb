8a5775773a22b23a22525b2fc05d997ad2a1f940
Dmitry Ponyatov <dponyatov@gmail.com>
2023-10-24 19:56:09 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/app.d
+++ src/app.d
@@ -1,21 +1,29 @@
-import std.stdio;
+// import std.stdio;
 
 import vibe.vibe;
+
+class WebService {
+    @path("/") void index(HTTPServerRequest req, HTTPServerResponse res) {
+        logInfo("%s\t%s".format(req.method, req.requestURI));
+        res.render!("index.dt", req);
+    }
+}
 
 void main() {
     auto settings = new HTTPServerSettings;
     settings.port = 8888;
     settings.bindAddresses = ["127.0.0.1"];
 
-    // auto router = new URLRouter;
-    // router.get("/", &index);
+    auto router = new URLRouter;
+    router.registerWebInterface(new WebService);
+    router.rebuild();
 
-    listenHTTP(settings, &index);
+    listenHTTP(settings, router);
 
     runApplication();
 }
 
-void index(HTTPServerRequest req, HTTPServerResponse res) {
-    logInfo("%s\t%s".format(req.method, req.requestURI));
-    // res.render!("index.dt", req);
-}
+// void index(HTTPServerRequest req, HTTPServerResponse res) {
+//     res.
+//     
+// }
